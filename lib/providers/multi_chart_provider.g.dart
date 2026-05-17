7b7030225e66670f5f89ad3089a7971e2abda70a
krzysztof.hofman <khofman250377@gmail.com>
2026-05-17 18:00:59 +0200

    fix: add generated multi chart provider's class

---
+++ lib/providers/multi_chart_provider.g.dart
@@ -0,0 +1,294 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'multi_chart_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, type=warning
+
+@ProviderFor(MultiChartNotifier)
+final multiChartProvider = MultiChartNotifierFamily._();
+
+final class MultiChartNotifierProvider
+    extends $NotifierProvider<MultiChartNotifier, MultiChartNotifierState> {
+  MultiChartNotifierProvider._({
+    required MultiChartNotifierFamily super.from,
+    required (CacheKeyType?, PeriodType?, bool?) super.argument,
+  }) : super(
+         retry: null,
+         name: r'multiChartProvider',
+         isAutoDispose: true,
+         dependencies: null,
+         $allTransitiveDependencies: null,
+       );
+
+  @override
+  String debugGetCreateSourceHash() => _$multiChartNotifierHash();
+
+  @override
+  String toString() {
+    return r'multiChartProvider'
+        ''
+        '$argument';
+  }
+
+  @$internal
+  @override
+  MultiChartNotifier create() => MultiChartNotifier();
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(MultiChartNotifierState value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<MultiChartNotifierState>(value),
+    );
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return other is MultiChartNotifierProvider && other.argument == argument;
+  }
+
+  @override
+  int get hashCode {
+    return argument.hashCode;
+  }
+}
+
+String _$multiChartNotifierHash() =>
+    r'd25c360aeecfbdfb8f96dd3da649caf5c36457ca';
+
+final class MultiChartNotifierFamily extends $Family
+    with
+        $ClassFamilyOverride<
+          MultiChartNotifier,
+          MultiChartNotifierState,
+          MultiChartNotifierState,
+          MultiChartNotifierState,
+          (CacheKeyType?, PeriodType?, bool?)
+        > {
+  MultiChartNotifierFamily._()
+    : super(
+        retry: null,
+        name: r'multiChartProvider',
+        dependencies: null,
+        $allTransitiveDependencies: null,
+        isAutoDispose: true,
+      );
+
+  MultiChartNotifierProvider call([
+    CacheKeyType? type,
+    PeriodType? periodType,
+    bool? keepAlive,
+  ]) => MultiChartNotifierProvider._(
+    argument: (type, periodType, keepAlive),
+    from: this,
+  );
+
+  @override
+  String toString() => r'multiChartProvider';
+}
+
+abstract class _$MultiChartNotifier extends $Notifier<MultiChartNotifierState> {
+  late final _$args = ref.$arg as (CacheKeyType?, PeriodType?, bool?);
+  CacheKeyType? get type => _$args.$1;
+  PeriodType? get periodType => _$args.$2;
+  bool? get keepAlive => _$args.$3;
+
+  MultiChartNotifierState build([
+    CacheKeyType? type,
+    PeriodType? periodType,
+    bool? keepAlive,
+  ]);
+  @$mustCallSuper
+  @override
+  void runBuild() {
+    final ref =
+        this.ref as $Ref<MultiChartNotifierState, MultiChartNotifierState>;
+    final element =
+        ref.element
+            as $ClassProviderElement<
+              AnyNotifier<MultiChartNotifierState, MultiChartNotifierState>,
+              MultiChartNotifierState,
+              Object?,
+              Object?
+            >;
+    element.handleCreate(ref, () => build(_$args.$1, _$args.$2, _$args.$3));
+  }
+}
+
+@ProviderFor(sortedMultiCharts)
+final sortedMultiChartsProvider = SortedMultiChartsFamily._();
+
+final class SortedMultiChartsProvider
+    extends
+        $FunctionalProvider<
+          List<MultiChartConfig>,
+          List<MultiChartConfig>,
+          List<MultiChartConfig>
+        >
+    with $Provider<List<MultiChartConfig>> {
+  SortedMultiChartsProvider._({
+    required SortedMultiChartsFamily super.from,
+    required CacheKeyType? super.argument,
+  }) : super(
+         retry: null,
+         name: r'sortedMultiChartsProvider',
+         isAutoDispose: true,
+         dependencies: null,
+         $allTransitiveDependencies: null,
+       );
+
+  @override
+  String debugGetCreateSourceHash() => _$sortedMultiChartsHash();
+
+  @override
+  String toString() {
+    return r'sortedMultiChartsProvider'
+        ''
+        '($argument)';
+  }
+
+  @$internal
+  @override
+  $ProviderElement<List<MultiChartConfig>> $createElement(
+    $ProviderPointer pointer,
+  ) => $ProviderElement(pointer);
+
+  @override
+  List<MultiChartConfig> create(Ref ref) {
+    final argument = this.argument as CacheKeyType?;
+    return sortedMultiCharts(ref, argument);
+  }
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(List<MultiChartConfig> value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<List<MultiChartConfig>>(value),
+    );
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return other is SortedMultiChartsProvider && other.argument == argument;
+  }
+
+  @override
+  int get hashCode {
+    return argument.hashCode;
+  }
+}
+
+String _$sortedMultiChartsHash() => r'61bd286a56148de96ba3326296c6090425659756';
+
+final class SortedMultiChartsFamily extends $Family
+    with $FunctionalFamilyOverride<List<MultiChartConfig>, CacheKeyType?> {
+  SortedMultiChartsFamily._()
+    : super(
+        retry: null,
+        name: r'sortedMultiChartsProvider',
+        dependencies: null,
+        $allTransitiveDependencies: null,
+        isAutoDispose: true,
+      );
+
+  SortedMultiChartsProvider call(CacheKeyType? type) =>
+      SortedMultiChartsProvider._(argument: type, from: this);
+
+  @override
+  String toString() => r'sortedMultiChartsProvider';
+}
+
+@ProviderFor(multiChartsBy)
+final multiChartsByProvider = MultiChartsByFamily._();
+
+final class MultiChartsByProvider
+    extends
+        $FunctionalProvider<
+          List<MultiChartConfig>,
+          List<MultiChartConfig>,
+          List<MultiChartConfig>
+        >
+    with $Provider<List<MultiChartConfig>> {
+  MultiChartsByProvider._({
+    required MultiChartsByFamily super.from,
+    required (CacheKeyType?, AssetConfig, PeriodType) super.argument,
+  }) : super(
+         retry: null,
+         name: r'multiChartsByProvider',
+         isAutoDispose: true,
+         dependencies: null,
+         $allTransitiveDependencies: null,
+       );
+
+  @override
+  String debugGetCreateSourceHash() => _$multiChartsByHash();
+
+  @override
+  String toString() {
+    return r'multiChartsByProvider'
+        ''
+        '$argument';
+  }
+
+  @$internal
+  @override
+  $ProviderElement<List<MultiChartConfig>> $createElement(
+    $ProviderPointer pointer,
+  ) => $ProviderElement(pointer);
+
+  @override
+  List<MultiChartConfig> create(Ref ref) {
+    final argument = this.argument as (CacheKeyType?, AssetConfig, PeriodType);
+    return multiChartsBy(ref, argument.$1, argument.$2, argument.$3);
+  }
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(List<MultiChartConfig> value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<List<MultiChartConfig>>(value),
+    );
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return other is MultiChartsByProvider && other.argument == argument;
+  }
+
+  @override
+  int get hashCode {
+    return argument.hashCode;
+  }
+}
+
+String _$multiChartsByHash() => r'4f36a73897394ff768cec59c4cfef9182db8b0c8';
+
+final class MultiChartsByFamily extends $Family
+    with
+        $FunctionalFamilyOverride<
+          List<MultiChartConfig>,
+          (CacheKeyType?, AssetConfig, PeriodType)
+        > {
+  MultiChartsByFamily._()
+    : super(
+        retry: null,
+        name: r'multiChartsByProvider',
+        dependencies: null,
+        $allTransitiveDependencies: null,
+        isAutoDispose: true,
+      );
+
+  MultiChartsByProvider call(
+    CacheKeyType? type,
+    AssetConfig asset,
+    PeriodType periodType,
+  ) => MultiChartsByProvider._(argument: (type, asset, periodType), from: this);
+
+  @override
+  String toString() => r'multiChartsByProvider';
+}
