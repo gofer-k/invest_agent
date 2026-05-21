1fc332d97fd61d7bcc5d899dc3f96167e1db420f
krzysztof.hofman <khofman250377@gmail.com>
2026-05-21 13:31:31 +0200

    chore: more detailed used providers' details

--- lib/providers/logger_riverpod.dart
+++ lib/providers/logger_riverpod.dart
@@ -6,15 +6,52 @@ final class LoggerRiverpod extends ProviderObserver{
   const LoggerRiverpod();
 
   @override
+  void didAddProvider(ProviderObserverContext context, Object? value) {
+    if (kDebugMode) {
+      print('''
+    {
+    "added": "${context.provider.name}",
+    "argument:": "${context.provider.argument}",
+    "value": "$value",
+    }
+    ''');
+    }
+  }
+
+  @override
   void didUpdateProvider(
       ProviderObserverContext context, Object? previousValue, Object? newValue) {
     if (kDebugMode) {
       print('''
     {
-    "provider": "${context.provider.name}",
-    "oldValue": "$previousValue",
-    "newValue": "$newValue",
+    "update": "${context.provider.name}",
+    "argument:": "${context.provider.argument}",
     mutation: "${context.mutation}"    
+    }
+    ''');
+    }
+  }
+
+  @override
+  void didDisposeProvider(ProviderObserverContext context) {
+    if (kDebugMode) {
+      print('''
+    {
+    "disposal": "${context.provider.name}",
+    "argument:": "${context.provider.argument}",
+    }
+    ''');
+    }
+  }
+
+  @override
+  void providerDidFail(ProviderObserverContext context, Object error, StackTrace _) {
+    if (kDebugMode) {
+      print('''
+    {
+    "fail": "${context.provider.name}",
+    "argument:": "${context.provider.argument}",
+    "error": "$error",
     }
     ''');
     }
